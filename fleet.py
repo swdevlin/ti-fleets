74e2d413b564c18ea83444fa2ee6cf1f75677eb8
Shawn Devlin <swdevlin@gmail.com>
2018-04-07 18:09:19 -0400

    Start on the races

--- fleet.py
+++ fleet.py
@@ -1,35 +1,93 @@
 class Ship(object):
-	def __init__(self, name, cost, hit_on, capacity=0, rolls=1, sustain=0):
+	def __init__(self, name, cost, hit_on, capacity=0, rolls=1, sustain=0, race='', tech=1):
 		self.name = name
 		self.capacity = capacity
 		self.cost = cost
 		self.hit_on = hit_on
 		self.rolls = rolls
 		self.sustain = sustain
+		self.race = race
+		self.tech = tech
+		self.cannons = {'rolls': 0, 'hit_on': 10}
+		self.ship_buff = {'rolls': 0, 'hit_on': 0}
+		if race == 'sardakk':
+			self.hit_on = self.hit_on - 1
+
+	def hits(self):
+		return (10.0 - self.hit_on) / 10.0 * self.rolls
+
 
 class Flagship(Ship):
 	def __init__(self, race='', tech=1):
-		if race == 'hacan':
+		if race == '':
+			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=7, rolls=2, capacity=2)
+		elif race == 'arborec':
+			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=7, rolls=2, capacity=5)
+		elif race == 'creuss':
+			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=5, rolls=1, capacity=3)
+		elif race == 'hacan':
 			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=7, rolls=2, capacity=3)
+		elif race == 'jol-nar':
+			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=6, rolls=2, capacity=5)
+		elif race == 'l1z1x':
+			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=5, rolls=2, capacity=5)
+		elif race == 'letnev':
+			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=5, rolls=2, capacity=3)
+		elif race == 'mentak':
+			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=7, rolls=2, capacity=3)
+		elif race == 'muaat':
+			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=5, rolls=2, capacity=3)
 		elif race == 'naalu':
 			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=9, rolls=2, capacity=6)
-		elif race == 'yssaril':
-			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=5, rolls=2, capacity=6)
+		elif race == 'nekro':
+			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=9, rolls=2, capacity=3)
 		elif race == 'saar':
 			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=5, rolls=2, capacity=3)
+		elif race == 'sardakk':
+			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=6, rolls=2, capacity=3)
+			self.ship_buff = {'rolls': 0, 'hit_on': 1}
+		elif race == 'sol':
+			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=5, rolls=2, capacity=12)
+		elif race == 'winnu':
+			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=7, rolls=1, capacity=3)
+		elif race == 'xxcha':
+			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=7, rolls=2, capacity=3)
+			self.cannons = {'rolls': 3, 'hit_on': 5}
+		elif race == 'yin':
+			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=9, rolls=2, capacity=3)
+		elif race == 'yssaril':
+			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=5, rolls=2, capacity=6)
 		else:
-			super().__init__(self.__class__.__name__, cost=8, sustain=1, hit_on=7, rolls=2, capacity=2)
+			raise Exception('Invalid race')
+
+	def hits(self):
+		hits = super().hits()
+		if self.race == 'jol-nar':
+			hits = hits + 0.2*2
+		return hits
+
 
 class Warsun(Ship):
 	def __init__(self, race='', tech=1):
-		super().__init__(self.__class__.__name__, cost=12, hit_on=3, capacity=6, rolls=3, sustain=1)
+		if race == 'muaat':
+			if tech == 2:
+				super().__init__(self.__class__.__name__, cost=10, hit_on=3, capacity=6, rolls=3, sustain=1)
+			else:
+				super().__init__(self.__class__.__name__, cost=12, hit_on=3, capacity=6, rolls=3, sustain=1)
+		else:
+			super().__init__(self.__class__.__name__, cost=12, hit_on=3, capacity=6, rolls=3, sustain=1)
+
 
 class Dreadnought(Ship):
 	def __init__(self, race='', tech=1):
 		if tech == 2:
-			super().__init__(self.__class__.__name__, cost=4, hit_on=5, capacity=1, rolls=1, sustain=1)
+			if race == 'l1z1x':
+				super().__init__(self.__class__.__name__, cost=4, hit_on=4, capacity=2, rolls=1, sustain=1)
+			else:
+				super().__init__(self.__class__.__name__, cost=4, hit_on=5, capacity=1, rolls=1, sustain=1)
 		else:
 			super().__init__(self.__class__.__name__, cost=4, hit_on=5, capacity=1, rolls=1, sustain=1)
+
 
 class Cruiser(Ship):
 	def __init__(self, race='', tech=1):
@@ -38,12 +96,14 @@ class Cruiser(Ship):
 		else:
 			super().__init__(self.__class__.__name__, cost=2, hit_on=7, capacity=0, rolls=1, sustain=0)
 
+
 class Fighter(Ship):
 	def __init__(self, race='', tech=1):
 		if tech == 2:
 			super().__init__(self.__class__.__name__, cost=0.5, hit_on=8, capacity=0, rolls=1, sustain=0)
 		else:
 			super().__init__(self.__class__.__name__, cost=0.5, hit_on=9, capacity=0, rolls=1, sustain=0)
+
 
 class Destroyer(Ship):
 	def __init__(self, race='', tech=1):
@@ -52,12 +112,20 @@ class Destroyer(Ship):
 		else:
 			super().__init__(self.__class__.__name__, cost=1, hit_on=9, capacity=0, rolls=1, sustain=0)
 
+
 class Carrier(Ship):
 	def __init__(self, race='', tech=1):
 		if tech == 2:
-			super().__init__(self.__class__.__name__, cost=3, hit_on=9, capacity=6, rolls=1, sustain=0)
+			if race=='sol':
+				super().__init__(self.__class__.__name__, cost=3, hit_on=9, capacity=8, rolls=1, sustain=1)
+			else:
+				super().__init__(self.__class__.__name__, cost=3, hit_on=9, capacity=6, rolls=1, sustain=0)
 		else:
-			super().__init__(self.__class__.__name__, cost=3, hit_on=9, capacity=4, rolls=1, sustain=0)
+			if race=='sol':
+				super().__init__(self.__class__.__name__, cost=3, hit_on=9, capacity=4, rolls=1, sustain=0)
+			else:
+				super().__init__(self.__class__.__name__, cost=3, hit_on=9, capacity=6, rolls=1, sustain=0)
+
 
 class Fleet(object):
 	def __init__(self, ships):
@@ -82,6 +150,5 @@ class Fleet(object):
 			resilience = resilience + fighter.sustain + 1
 			cost = cost + fighter.cost
 			hits = hits + (10.0 - fighter.hit_on) / 10.0 * fighter.rolls
-			types.append(fighter.name[:2])
 
-		return ','.join([('.'.join(types)), str(cost), str(hits), str(hits / cost), str(resilience)])
+		return ','.join([('.'.join(types)), str(fighters), str(cost), str(hits), str(hits / cost), str(resilience)])
--- ti4.py
+++ ti4.py
@@ -2,6 +2,18 @@ import argparse
 
 from fleet import Flagship, Warsun, Dreadnought, Cruiser, Destroyer, Carrier, Fighter, Fleet
 
+FLAGSHIPS=1
+WARSUNS=2
+DREADNOUGHTS=5
+CRUISERS=8
+DESTROYERS=8
+CARRIERS=4
+
+RACES = [
+	'', 'arborec', 'creuss', 'hacan', 'jol-nar', 'l1z1x', 'letnev', 'mentak', 'muaat', 'naalu', 'nekro', 'saar',
+	'sardakk', 'sol', 'winnu', 'xxcha', 'yin', 'yssaril'
+]
+
 
 class tech2(argparse.Action):
 	def __call__(self, parser, namespace, values, option_string=None):
@@ -9,7 +21,7 @@ class tech2(argparse.Action):
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--race", help="Race for the fleet", default='')
+parser.add_argument("--race", help="Race for the fleet", choices=RACES, default='')
 parser.add_argument("--size", type=int, help="fleet size", default=1)
 parser.add_argument("--percent", type=int, help="precentage of capacity that is fighters", default=0, choices=range(0,101))
 parser.add_argument("-de", "--destroyer", help="destroyers are tech 2", action=tech2, default=1)
@@ -21,26 +33,31 @@ parser.add_argument("-dr", "--dreadnought", help="dreadnoughts are tech 2", acti
 parser.add_argument("--debug", help="debug mode", action="store_true")
 args = parser.parse_args()
 
-race = ''
 flagship = Flagship(race=args.race, tech=1)
-warsun = Warsun(race=race, tech=args.warsun)
-dreadnought = Dreadnought(race=race, tech=args.dreadnought)
-cruiser = Cruiser(race=race, tech=args.cruiser)
-destroyer = Destroyer(race=race, tech=args.destroyer)
-carrier = Carrier(race=race, tech=args.carrier)
-fighter = Fighter(race=race, tech=args.fighter)
+warsun = Warsun(race=args.race, tech=args.warsun)
+dreadnought = Dreadnought(race=args.race, tech=args.dreadnought)
+cruiser = Cruiser(race=args.race, tech=args.cruiser)
+destroyer = Destroyer(race=args.race, tech=args.destroyer)
+carrier = Carrier(race=args.race, tech=args.carrier)
+fighter = Fighter(race=args.race, tech=args.fighter)
 
-available_untis = [
-	flagship,
-	warsun, warsun,
-	dreadnought, dreadnought, dreadnought, dreadnought, dreadnought,
-	cruiser, cruiser, cruiser, cruiser, cruiser, cruiser, cruiser, cruiser,
-	destroyer, destroyer, destroyer, destroyer, destroyer, destroyer, destroyer, destroyer,
-	carrier, carrier, carrier, carrier
-]
+available_units = []
+
+for _ in range(FLAGSHIPS):
+	available_units.append(flagship)
+for _ in range(WARSUNS):
+	available_units.append(warsun)
+for _ in range(DREADNOUGHTS):
+	available_units.append(dreadnought)
+for _ in range(CRUISERS):
+	available_units.append(cruiser)
+for _ in range(DESTROYERS):
+	available_units.append(destroyer)
+for _ in range(CARRIERS):
+	available_units.append(carrier)
+fighter = Fighter(race=args.race, tech=args.fighter)
 
 fleets = set()
-
 
 def add_ship(fleet, pool):
 	if len(pool) == 0:
@@ -62,25 +79,35 @@ def fleet_dump(fleet, fighter_capacity=0):
 	resilience = 0
 
 	for ship in fleet:
+		if ship.ship_buff['rolls'] > 0:
+			for other in fleet:
+				if other != ship:
+					other.rolls = other.rolls + ship.ship_buff['rolls']
+		if ship.ship_buff['to_hit'] != 0:
+			for other in fleet:
+				if other != ship:
+					other.rolls = other.rolls - ship.ship_buff['to_hit']
+
+	for ship in fleet:
 		ships.append(ship.name)
 		cost = cost + ship.cost
 		capacity = capacity + ship.capacity
 		resilience = resilience + ship.sustain + 1
-		hits = hits + (10.0 - ship.hit_on) / 10.0 * ship.rolls
+		hits = hits + ship.hits()
 	fighters = int(capacity * fighter_capacity / 100)
 	for _ in range(fighters):
 		resilience = resilience + fighter.sustain + 1
 		cost = cost + fighter.cost
-		hits = hits + (10.0 - fighter.hit_on) / 10.0 * fighter.rolls
+		hits = hits + fighter.hits()
 		ships.append(fighter.name[:2])
 
 	return ','.join(['.'.join(ships), cost, hits, hits / cost, resilience])
 
 
-for _ in range(len(available_untis) + 1):
+for _ in range(len(available_units) + 1):
 	if args.debug:
 		print('computing', _)
-	pool = available_untis[_:]
+	pool = available_units[_:]
 	if len(pool) >= args.size:
 		add_ship([], pool)
 
@@ -88,8 +115,13 @@ final = []
 for fleet in fleets:
 	final.append(list(fleet))
 
+breakdown = []
 for f in final:
 	fleet = Fleet(f)
-	print(fleet.breakdown(fighter_capacity=args.percent, fighter=fighter))
+	breakdown.append(fleet.breakdown(fighter_capacity=args.percent, fighter=fighter))
 
+print('Fleet,Fighters,Cost,Hits in Round,Hits/Cost,Resilience')
+print('\n'.join(sorted(breakdown)))
 print(len(final), " total fleets")
+
+
